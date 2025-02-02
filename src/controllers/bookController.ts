ac145fb7345d4142490ccc1cb5fba868d53db6f4
Christian Martin <cmartin60@academic.rrc.ca>
2025-02-01 21:23:53 -0600

    Created branch for ticket 5. updated codes for Modifying books. added a published date.

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -24,15 +24,14 @@ export const updateBook = (req: Request, res: Response): void => {
     try {
         const { id } = req.params;
         const updatedData = req.body;
+
+        // You can further validate or sanitize updatedData if necessary
+
         const updatedBook = bookService.updateBook(id, updatedData);
-        if (updatedBook) {
-            res.status(200).json({
-                message: "Book updated",
-                data: updatedBook,
-            });
-        } else {
-            res.status(404).json({ message: "Book not found" });
-        }
+        res.status(200).json({
+            message: "Book updated",
+            data: updatedBook,
+        });
     } catch (error) {
         res.status(500).json({ message: "Error updating book" });
     }
